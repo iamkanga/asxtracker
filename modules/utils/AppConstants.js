b0fedfdffa9417fdd9a4d867d4107f306861c30b
Kanga <iamkanga@gmail.com>
2026-07-04 10:25:53 +0700

    feat: initialize AppConstants.js with centralized application configuration and event definitions

--- modules/utils/AppConstants.js
+++ modules/utils/AppConstants.js
@@ -3,7 +3,7 @@
  * Centralized constants for the application.
  */
 
-export const APP_VERSION = 'v1.2.14';
+export const APP_VERSION = '1.24';
 export const ALL_SHARES_ID = 'ALL';
 export const KANGAROO_ICON_SRC = 'favicon.svg';
 export const CASH_WATCHLIST_ID = 'CASH';
